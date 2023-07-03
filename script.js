7be208612f298b1670d7c6b64b5d968f262e2ea4
Diogo Henrique <diogohs110@gmail.com>
2023-07-02 22:15:44 -0300

    Added style

---
+++ script.js
@@ -0,0 +1,23 @@
+let number1 = 50;
+let number2 = 4;
+let operator;
+
+function operate(operator, n1, n2) {
+  return add(n1, n2);
+}
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
